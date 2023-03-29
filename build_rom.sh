8018922c64d8d093556fc689066b21a7ec6e3e5c
sdtuif <118095651+sdtuif@users.noreply.github.com>
2023-03-29 23:37:10 +0600

    lavender-nusantara-sa
    Testing

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # add repo,dt, vt, kt etc
-repo init --depth=1 --no-repo-verify -u https://github.com/Sa-Sajjad/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/qwry3657/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
