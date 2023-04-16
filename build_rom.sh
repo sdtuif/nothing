b3acab14b8e900981062467cdfa4153f569ab758
sdtuif <118095651+sdtuif@users.noreply.github.com>
2023-04-16 23:07:57 +0600

    lavender-nusantara-test

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,20 @@
 # add repo,dt, vt, kt etc
 repo init --depth=1 --no-repo-verify -u https://github.com/Sa-Sajjad/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
+# replace with your manifest
 git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#  rom
-# don't touch those command
+# fill up
 source build/envsetup.sh
+# replace with your rom variable
 lunch nad_lavender-userdebug
+# time zone
 export TZ=Asia/Dhaka
+# don't touch
 make api-stubs-docs || echo no problem
 make system-api-stubs-docs || echo no problem
 make test-api-stubs-docs || echo no problem
+# replace with your rom variable
 mka nad
+# idk what is this
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
