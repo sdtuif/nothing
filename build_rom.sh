08c9065090c0479b121d3fe90b48bbb5957f2ad6
Sa Sajjad <sqsa93103@gmail.com>
2023-05-07 09:27:02 +0600

    Reflector code

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Sa-Sajjad/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
 # replace with your manifest
 git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b 1 .repo/local_manifests
+# sync script
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # fill up
@@ -17,4 +18,4 @@ make test-api-stubs-docs || echo no problem
 # replace with your rom variable
 mka nad
 # idk what is this
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/$rom_name/*.zip cirrus:$rom_name -P
